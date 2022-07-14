3c37280dad0880fe5040965ab4f787059620646d
Yuxj-China <56662738+Yuxj-China@users.noreply.github.com>
2022-07-14 16:21:49 +0800

    web源文件及用户图片数据

---
+++ www/php/delete.php
@@ -0,0 +1,18 @@
+<?php
+    include('connect.php');
+    
+    $addr=$_POST['address'];
+    
+    $sql="DELETE from image where address='$addr'";
+    mysqli_query($con, $sql);
+    
+    if(mysqli_query($con, $sql) && unlink($addr)){ //删除服务器和数据库中图片的痕迹
+        alert("删除成功", "home.php");
+    }
+    else{
+        alert("删除失败", "home.php");
+    }
+    
+    
+
+
