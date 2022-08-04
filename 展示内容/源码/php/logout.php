5eaa16d35e78730f004e22de68d5b7aa38e394f8
padresvater <1649299415@qq.com>
2022-08-04 14:19:24 +0800

    BreakIt基础框架

--- 展示内容/源码/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('connect.php');
-    session_start();
-    unset($_SESSION['username']); //销毁会话
-    session_destroy();
-//    echo '<script>location.href="../index.html";</script>';
-    header('Location: ../index.html'); //跳转登录页面
-?>
